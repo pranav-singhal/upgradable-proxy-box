8d61843db3c8f871136a7191c19f649e5a49699c
arvindkalra <kalarvind97@gmail.com>
2019-10-12 17:21:43 +0530

    updated Readme

--- migrations/5_deploy_send.js
+++ migrations/5_deploy_send.js
@@ -6,9 +6,7 @@ module.exports = function(deployer, network, accounts) {
   TokenStorage.at(TokenStorage.address).then(storage => {
     storage["setProxyContract"](TokenProxy.address).then(() => {
       Token_V0.at(TokenProxy.address).then(v0 => {
-        v0["setInitials"](1000).then(() => {
-          console.log("Initial Supply Set");
-        });
+        v0["setInitials"](1000).then(() => {});
       });
     });
   });
